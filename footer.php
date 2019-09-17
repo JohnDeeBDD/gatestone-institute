33ba4f189685a78d384baec602f45bb6795eb4de
Jim Karlinski <ubuntu@ip-172-31-34-124.ec2.internal>
2019-09-17 03:51:53 +0000

    wtf

---
+++ footer.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="wrap">
+<div style="padding:20px 30px;">
+
+  <p>Copyright &copy; 2019 Gatestone Institute. <br />All rights reserved.</p>
+
+<p>The articles printed here do not necessarily reflect the views of the Editors or of Gatestone Institute. 
+Both reserve the right not to publish replies to articles should they so choose.<br />
+Gatestone Institute is a 501(c)3 not-for-profit organization, Federal Tax ID #454724565.</p>
+
+<div class="title"><b>Gatestone Contacts</b></div>
+  <p>Contact: info@gatestoneinstitute.org<br />
+    Editorial: editor@gatestoneinstitute.org<br />
+Comments: support@parler.com</p>
+
+<p><a href="https://www.gatestoneinstitute.org/terms-of-use/">Terms of Use</a> &nbsp; <a href="https://www.gatestoneinstitute.org/privacy-policy/">Privacy &amp; Cookies Policy</a></p>
+
+</div>
+			</div><!-- .wrap -->
+		</footer><!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -14,29 +14,21 @@
  * @since 1.0
  * @version 1.0
  */
-
 get_header(); ?>
-
 <div class="wrap">
-	<?php if ( is_home() && ! is_front_page() ) : ?>
+<?php	
+ if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
 			<h1 class="page-title"><?php single_post_title(); ?></h1>
 		</header>
 	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
-	</header>
 	<?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-				
-
 			<!-- blog section -->
-
 			<!-- Check for posts 	 -->
-			<?php
+<?php 
                 if( have_posts()):
                 while(have_posts()): the_post();
             ?>
